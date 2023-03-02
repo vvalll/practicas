0b131c70f1eace91f673063445530d117d38ad82
vvalll <valeng22092@gmail.com>
2023-03-02 16:33:50 -0500

    actualizacion 02/03

---
+++ funciones.py
@@ -0,0 +1,32 @@
+#----
+'''
+def acumulador(a,b):
+    sum = 0
+    for x in range(a,b):
+        sum += x
+    return sum
+
+print(acumulador(2,6))
+'''
+
+#----
+'''
+def volumen(width, depth = 2,lenght=3):
+    return lenght * width * depth
+
+result = volumen(width=2)
+print(result)
+'''
+
+#---
+
+price = 100
+
+def prueba():
+    prices = price + 10
+    print(prices)
+    edad = 18
+    print(edad)
+
+print(price)
+prueba()
---
+++ juego.py
@@ -0,0 +1,39 @@
+import random
+def juego ():
+    ronda = 0
+    maquina = 0 
+    usuario = 0
+    for x in range(1,4):
+        ronda+=1
+        print("************")
+        print("RONDA: ", ronda)
+        print("************")
+        res = [ ]
+        elementos = ["piedra", "papel", "tijera"]
+        user = input("Escribe tu eleccion (piedra, papel o tijera): ")
+        res.append(random.choice(elementos))
+        res.append(user)
+
+        if res[0] == res[1] or res[1] == res [0]:
+            print("Maquina: ", res[0], "// Jugador: ", res[1])
+            print("EMPATE!")
+            print("MAQUINA: ", maquina, " // JUGADOR: ", usuario)
+
+        elif res[0] == "piedra" and res[1] == "tijera" or res[0] == "papel" and res[1] == "piedra" or res[0] == "tijera" and res[1] == "papel":
+            print("Maquina: ", res[0], "// Jugador: ", res[1]) 
+            maquina+=1
+            print("MAQUINA: ", maquina, " // JUGADOR: ", usuario)
+
+        elif res[0] == "piedra" and res[1] == "papel" or res[0] == "papel" and res[1] == "tijera" or res[0] == "tijera" and res[1] == "piedra":
+            print("Maquina: ", res[0], "// Jugador: ", res[1])
+            usuario+=1
+            print("MAQUINA: ", maquina, " // JUGADOR: ", usuario)
+
+        if maquina == 3:
+            print("Gana la maquina")
+        elif usuario == 3: 
+            print("Gana el jugador, Ganaste!")
+
+
+
+juego()
